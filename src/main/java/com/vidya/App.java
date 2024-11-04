f1cd6c1e110cc508e9cad4de567ea8271464a5aa
vidya-source <68222679+vidya-source@users.noreply.github.com>
2024-11-04 22:34:27 +0530

    Update App.java

--- src/main/java/com/vidya/App.java
+++ src/main/java/com/vidya/App.java
@@ -5,6 +5,6 @@ package com.vidya;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Vidya!");
     }
 }
